70718216162597355502a54a6d55484df420a987
fabian <fabian@einzelmensch.de>
2025-03-11 22:05:47 +0100

    improve not found ui

--- app/components/NotFound.tsx
+++ app/components/NotFound.tsx
@@ -1,22 +1,16 @@
+import type { PropsWithChildren } from 'react'
 import { Link } from '@tanstack/react-router'
 
-export function NotFound({ children }: { children?: any }) {
+export function NotFound({ children }: PropsWithChildren) {
   return (
-    <div className="space-y-2 p-2">
-      <div className="text-gray-600 dark:text-gray-400">
+    <div className="p-2 lg:container mx-auto">
+      <div className="text-gray-600 dark:text-gray-400 mb-2">
         {children || <p>The page you are looking for does not exist.</p>}
       </div>
       <p className="flex items-center gap-2 flex-wrap">
-        <button
-          type="button"
-          onClick={() => window.history.back()}
-          className="bg-emerald-500 text-white px-2 py-1 rounded-sm uppercase font-black text-sm"
-        >
-          Go back
-        </button>
         <Link
           to="/"
-          className="bg-cyan-600 text-white px-2 py-1 rounded-sm uppercase font-black text-sm"
+          className="btn btn-primary btn-outline"
         >
           Start Over
         </Link>
